7885c15585f8873474d1d7c77b0fde27b1ed7cf2
Stefano Corra <stefano.corra@hotmail.com>
2017-11-18 18:10:02 -0500

    second commit

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,16 @@
+import React, {Component,PropTypes} from 'react';
+
+
+class CoursesPage extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Courses</h1>
+      </div>
+    );
+  }
+
+}
+
+export default CoursesPage;
--- src/index.test.js
+++ src/index.test.js
@@ -2,6 +2,6 @@ import expect from 'expect';
 
 describe('Our first test', () => {
   it('should pass', () => {
-    expect(false).toEqual(true);
+    expect(true).toEqual(true);
   });
 });

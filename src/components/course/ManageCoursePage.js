5b44e4e0564fefb0862eb2d6e00af04789cd2f41
Stefano Corra <stefano.corra@hotmail.com>
2017-11-22 16:53:36 -0500

    sixth commit

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -12,30 +12,48 @@ export class ManageCoursePage extends Component {
     course: Object.assign({}, props.course),
     errors: {}
   };
+  this.updateCourseState = this.updateCourseState.bind(this);
 }
+
+updateCourseState(event) {
+  const field = event.target.name;
+  let course = Object.assign({}, this.state.course);
+  course[field] = event.target.value;
+  return this.setState({course: course});
+}
+
   render() {
     return (
-      <div>
+
         <CourseForm
-          allAuthors={[]}
+          allAuthors={this.props.authors}
+          onChange={this.updateCourseState}
           course={this.state.course}
           errors={this.state.errors}
         />
-      </div>
+
     );
   }
 }
 
 ManageCoursePage.propTypes = {
   course: PropTypes.object.isRequired,
-  authors: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired
+  authors: PropTypes.array.isRequired
+//  actions: PropTypes.object.isRequired
 };
 
- function mapStateToProps({state,ownProps}) {
+ function mapStateToProps(state,ownProps) {
      let course = {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''};
+     const authorsFormattedForDropdown = state.authors.map(author =>{
+       return{
+         value: author.id,
+         text: author.firstName + ' ' + author.lastName
+       };
+     });
+
   return{
-    course:course
+    course:course,
+    authors: authorsFormattedForDropdown
   };
 }
 

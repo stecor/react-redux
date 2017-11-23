903c026e8fc1317f36d8725aaa7b1a0bacb2be7d
Stefano Corra <stefano.corra@hotmail.com>
2017-11-22 22:12:14 -0500

    nineth commit

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -16,6 +16,13 @@ export class ManageCoursePage extends Component {
   this.saveCourse = this.saveCourse.bind(this);
 }
 
+componentWillReceiveProps(nextProps) {
+  if (this.props.course.id != nextProps.course.id) {
+    // Necessary to populate form when existing course is loaded directly.
+    this.setState({course: Object.assign({}, nextProps.course)});
+  }
+}
+
 updateCourseState(event) {
   const field = event.target.name;
   let course = Object.assign({}, this.state.course);
@@ -56,17 +63,19 @@ ManageCoursePage.contextTypes = {
 };
 
 function getCourseById(courses, id) {
+
   const course = courses.filter(course => course.id == id);
   if (course.length) return course[0]; //since filter returns an array, have to grab the first.
   return null;
 }
+
 
  function mapStateToProps(state,ownProps) {
 
     const courseId = ownProps.params.id; // from the path `/course/:id`
     let course = {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''};
 
-    if (courseId){
+    if (courseId && state.courses.length > 0){
       course = getCourseById(state.courses,courseId);
     }
     const authorsFormattedForDropdown = state.authors.map(author =>{

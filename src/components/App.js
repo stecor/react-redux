5251e910dbbf902b6d50e6bde7e7577e0ad6bfa3
Stefano Corra <stefano.corra@hotmail.com>
2017-11-23 22:50:57 -0500

    tenth commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 // This component handles the app templare used on every page.
 import React, { Component, PropTypes } from 'react';
 import Header from './common/Header';
+import {connect} from 'react-redux';
 
 class App extends Component {
 
   render() {
     return (
       <div className="container-fluid">
-        <Header />
+        <Header
+            loading={this.props.loading}
+          />
         {this.props.children}
       </div>
     );
@@ -15,6 +18,14 @@ class App extends Component {
 }
 
 App.propTypes ={
-  children: PropTypes.object.isRequired
+  children: PropTypes.object.isRequired,
+  loading: PropTypes.bool.isRequired
 };
-export default App;
+
+function mapStateToProps(state, ownProps) {
+  return {
+    loading: state.ajaxCallsInProgress > 0
+  };
+}
+
+export default connect(mapStateToProps)(App);
